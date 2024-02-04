329c9ac3f490f7febe73b32bfa37ce9500aa9861
Roopam123 <roopamr2411@gmail.com>
2024-02-04 19:38:48 +0530

    data Type in JavaScript

---
+++ dataType.js
@@ -0,0 +1,27 @@
+// Number
+
+let age = 23;
+// console.log(age);
+// console.log(typeof(age));
+
+let name = "Roopam";
+// console.log(name);
+// console.log(typeof(name));
+
+
+let isTrue = true;
+// console.log(isTrue);
+// console.log(typeof(isTrue));
+
+
+let isNull = null;
+// console.log(isNull);
+// console.log(typeof(isNull));
+
+let myAge
+// console.log(myAge);
+// console.log(typeof(myAge));
+
+
+let a = Symbol();
+console.log(typeof(a));
